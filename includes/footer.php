e8fb8f2743069e4fc4d0b4ec5be718bc4f5aa957
Dwg82 <duongba0802@gmail.com>
2025-04-03 07:28:15 +0700

    Add includes/footer.php 1743640011916

---
+++ includes/footer.php
@@ -0,0 +1,58 @@
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>About Us</h5>
+                <p>We provide the best laptops and computing solutions for all your needs at competitive prices.</p>
+                <div class="social-icons">
+                    <a href="#" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-white me-2"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="index.php" class="text-white">Home</a></li>
+                    <li><a href="products.php" class="text-white">Products</a></li>
+                    <li><a href="about.php" class="text-white">About</a></li>
+                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Customer Service</h5>
+                <ul class="list-unstyled">
+                    <li><a href="faq.php" class="text-white">FAQ</a></li>
+                    <li><a href="shipping.php" class="text-white">Shipping Policy</a></li>
+                    <li><a href="returns.php" class="text-white">Return Policy</a></li>
+                    <li><a href="privacy.php" class="text-white">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Contact Us</h5>
+                <ul class="list-unstyled">
+                    <li><i class="fas fa-map-marker-alt me-2"></i> 123 Laptop St, Tech City</li>
+                    <li><i class="fas fa-phone me-2"></i> (123) 456-7890</li>
+                    <li><i class="fas fa-envelope me-2"></i> info@laptopstore.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-4">
+        <div class="row">
+            <div class="col-md-6">
+                <p>&copy; <?= date('Y') ?> Laptop Store. All rights reserved.</p>
+            </div>
+            <div class="col-md-6 text-md-end">
+                <p>Accepted Payment Methods: 
+                    <i class="fab fa-cc-visa fa-lg ms-2"></i>
+                    <i class="fab fa-cc-mastercard fa-lg ms-2"></i>
+                    <i class="fab fa-cc-paypal fa-lg ms-2"></i>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
