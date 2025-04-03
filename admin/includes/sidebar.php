716f832165cd652a3cc5f740d52f2c5df948c9ca
Dwg82 <duongba0802@gmail.com>
2025-04-03 07:28:31 +0700

    Add admin/includes/sidebar.php 1743640028145

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,36 @@
+<div class="sidebar col-md-3 col-lg-2 d-md-block bg-dark collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="index.php">
+                    <i class="fas fa-tachometer-alt me-2"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="products.php">
+                    <i class="fas fa-laptop me-2"></i> Products
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="categories.php">
+                    <i class="fas fa-list me-2"></i> Categories
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="orders.php">
+                    <i class="fas fa-shopping-cart me-2"></i> Orders
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="users.php">
+                    <i class="fas fa-users me-2"></i> Users
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="reviews.php">
+                    <i class="fas fa-star me-2"></i> Reviews
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
